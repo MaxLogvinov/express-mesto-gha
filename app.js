c26e2a54d4c2f2389271cc7cbd55f13e1cfe20c6
Max Logvinov <maxlogvinov93@gmail.com>
2023-07-17 16:30:46 +0300

    Создание репозитория

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+
+const app = express();
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.log('Сервер успешно запущен');
+});
